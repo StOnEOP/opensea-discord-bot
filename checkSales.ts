82dd4789bf3e78f8bc281e9bb1224e71de411fe3
StOnEOP <jonasm10a@gmail.com>
2022-01-25 03:58:58 +0000

    Small fix

--- checkSales.ts
+++ checkSales.ts
@@ -4,18 +4,16 @@ import fetch from 'node-fetch';
 import { ethers } from "ethers";
 
 const OPENSEA_SHARED_STOREFRONT_ADDRESS = '0x495f947276749Ce646f68AC8c248420045cb7b5e';
-const my_id = '<@211950823314292748>'
+const my_id = '<@>'
 
 const discordBot = new Discord.Client();
-const  discordSetup = async (): Promise<TextChannel> => {
+const  discordSetup = async (channel: string): Promise<TextChannel> => {
+  const channelID = channel
   return new Promise<TextChannel>((resolve, reject) => {
-    ['DISCORD_BOT_TOKEN', 'DISCORD_CHANNEL_ID'].forEach((envVar) => {
-      if (!process.env[envVar]) reject(`${envVar} not set`)
-    })
-  
+    if (!process.env['DISCORD_BOT_TOKEN']) reject('DISCORD_BOT_TOKEN not set')
     discordBot.login(process.env.DISCORD_BOT_TOKEN);
     discordBot.on('ready', async () => {
-      const channel = await discordBot.channels.fetch(process.env.DISCORD_CHANNEL_ID!);
+      const channel = await discordBot.channels.fetch(channelID!);
       resolve(channel as TextChannel);
     });
   })
@@ -34,6 +32,7 @@ const buildMessage = (sale: any) => (
 		{ name: 'Buyer', value: sale?.winner_account?.address, },
 		{ name: 'Seller', value: sale?.seller?.address,  },
 	)
+  .setDescription(my_id)
   .setImage(sale.asset.image_url)
 	.setTimestamp(Date.parse(`${sale?.created_date}Z`))
 	.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
@@ -41,8 +40,7 @@ const buildMessage = (sale: any) => (
 
 async function main() {
   var binary = true;
-  const channel = await discordSetup();
-  const seconds = process.env.SECONDS ? parseInt(process.env.SECONDS) : 3_600;
+  const seconds = process.env.SECONDS ? parseInt(process.env.SECONDS) : 1000;
   const hoursAgo = (Math.round(new Date().getTime() / 1000) - (seconds)); // in the last hour, run hourly?
   var split = process.env.ADRESSES.split(",");
   var params = [];
@@ -51,6 +49,7 @@ async function main() {
   var i = 0
 
   for(i = 0; split.length > i; i++){
+    console.log(split[i]);
     //const params = new URLSearchParams({
     params[i] = new URLSearchParams({
       offset: '0',
@@ -65,31 +64,62 @@ async function main() {
       //params.append('asset_contract_address', process.env.CONTRACT_ADDRESS!)
     //}
 
-    openSeaResponse = await fetch(
-      "https://api.opensea.io/api/v1/events?" + params[i]).then((resp) => resp.json());
-      
-    console.log("recieved response")
-    await Promise.all(
-      openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
-        var j = 0;
-        for(j = 0; split.length > j; j++){
-          if (sale?.winner_account?.address === split[j]){
-            console.log("Entered");
-            binary = false;
-            const message = buildMessage(sale);
-            channel.send(my_id);
-            return await channel.send(message);
-          }
-        }
+    let responseText = "";
+
+    let openSeaFetch = {}
+    if (process.env.OPENSEA_TOKEN) {
+      openSeaFetch['headers'] = {'X-API-KEY': process.env.OPENSEA_TOKEN}
+    }
+
+    try {
+      const openSeaResponseObj = await fetch(
+        "https://api.opensea.io/api/v1/events?" + params[i], openSeaFetch
+      );
+
+      responseText = await openSeaResponseObj.text();
+      openSeaResponse = JSON.parse(responseText)
+        
+      console.log("recieved response")
+      await Promise.all(
+        openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
+          
+          if (sale.asset.name == null) sale.asset.name = 'Unnamed NFT';
+
+          const message = buildMessage(sale);
+          const channel = discordBot.channels.cache.get(my_id);
+          (channel as TextChannel).send(my_id);
+          binary = false;
+
+          return await Promise.all(
+            process.env.DISCORD_CHANNEL_ID.split(';').map(async (channel: string) => {
+              return await (await discordSetup(channel)).send(message)
+            })
+          );
+        })
+      );
+    } catch (e) {
+      const payload = responseText || "";
+
+      if (payload.includes("cloudflare") && payload.includes("1020")) {
+        throw new Error("You are being rate-limited by OpenSea. Please retrieve an OpenSea API token here: https://docs.opensea.io/reference/request-an-api-key")
+      }
+    
+      throw e;
+    }
+  }
+  if(binary){
+    return await Promise.all(
+      process.env.DISCORD_CHANNEL_ID.split(';').map(async (channel: string) => {
+        return await (await discordSetup(channel)).send('Nothing found')
       })
     );
   }
-  if (binary) await channel.send('Nothing found');
+  binary = true;
   return Promise.all;
 }
 
 main()
-  .then((res) =>{ 
+  .then((res) =>{
     if (!res.length) console.log("No recent sales");
     process.exit(0)
   })
